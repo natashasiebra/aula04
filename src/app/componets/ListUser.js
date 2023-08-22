11249c80824f4b46dff086d9b751cf43f2eed1c1
Kethiney <natashasiebra3@gmail.com>
2023-08-22 17:40:27 -0400

    commit

---
+++ src/app/componets/ListUser.js
@@ -0,0 +1,10 @@
+export default async function ListUser({users}){
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+    return(
+        <div>
+            {users?.map(user, index =>{
+                <p key={index}>{user.name}</p>
+            })}
+        </div>
+    )
+}
