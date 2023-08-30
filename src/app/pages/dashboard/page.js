4e930eb376ed4d807a4eabb92ac0d66e9165b3ed
natashasiebra <natashasiebra3@gmail.com>
2023-08-30 19:22:33 -0400

    commit

--- src/app/pages/dashboard/page.js
+++ src/app/pages/dashboard/page.js
@@ -1,6 +1,9 @@
 import { getUsers } from "@/app/functions/handlerAcessAPI";
 import { Suspense } from "react";
 import ListUser from "@/app/componets/ListUser";
+import Alter from "@/app/pages/alterar/pages"
+import Register from "@/app/pages/registrar/pages"
+
 
 export default async function Dashboard() {
    const users = await getUsers() 
@@ -8,6 +11,8 @@ export default async function Dashboard() {
         <div>
            <Suspense fallback={<p>carregando....</p>}>
             <ListUser users={users}/>
+            <button><a href={Alter}>alterar</a></button>
+            <button><a href={Register}>registrar</a></button>
            </Suspense>
             
         </div>
