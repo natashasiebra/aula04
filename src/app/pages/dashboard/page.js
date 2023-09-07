2fbb6bb408e6adf7c08c898b5566c72bee74b025
natashasiebra <natashasiebra3@gmail.com>
2023-09-07 15:06:37 -0400

    comit

--- src/app/pages/dashboard/page.js
+++ src/app/pages/dashboard/page.js
@@ -1,17 +1,21 @@
 import { getUsers } from "@/app/functions/handlerAcessAPI";
 import { Suspense } from "react";
 import ListUser from "@/app/componets/ListUser";
+import Cookies from "js-cookie";
 
-
-export default async function Dashboard() {
-   const users = await getUsers() 
+export default  function Dashboard() {
+const hancleAcessUser = () => {
+    Cookies.remove('token')
+}
+   const users =  getUsers() 
     return (
         <div>
            <Suspense fallback={<p>carregando....</p>}>
             <ListUser users={users}/>
             <a href="/pages/alterar">alterar</a>
             <a href="/pages/registrar">Registrar</a>
-
+            <button onClick={hancleAcessUser()}>remover
+            </button>
            </Suspense>
         </div>
     );
